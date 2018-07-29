0b384150fdee021865724ffc2059c8300f502f4c
arafat-213 <tai.arafat.at@gmail.com>
2018-07-29 13:38:27 +0530

    Practical 4 UI added

--- app/src/main/java/com/example/arafat_213/mcwclabpracticals/Practical4.java
+++ app/src/main/java/com/example/arafat_213/mcwclabpracticals/Practical4.java
@@ -1,40 +1,68 @@
 package com.example.arafat_213.mcwclabpracticals;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class Practical4 extends AppCompatActivity {
 
     private AutoCompleteTextView actCountry;
     private Spinner stateSpinner;
+    private EditText etName, etAge, etDOB, etEmail, etPhone;
+    private Button btnSubmit;
+    private CheckBox cbCoding, cbSinging, cbGaming;
+    private RadioGroup rgGender;
+    private Switch married;
     private String countries[] = {"India", "USA", "Canada", "Indonesia"};
     private String indiaStates[] = {"Gujarat", "Maharashtra", "Goa"};
     private String usaStates[] = {"New York", "Florida", "California"};
     private String canadaStates[] = {"Ontario"};
-
+    private String selectedState;
+    private String selectedCountry;
+    private RadioButton selectedGender;
+    private String selectedHobbies = " ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical4);
+
+        etName = findViewById(R.id.etName);
+        etDOB = findViewById(R.id.etDOB);
+        etAge = findViewById(R.id.etAge);
+        etEmail = findViewById(R.id.etEmail);
+        etPhone = findViewById(R.id.etPhone);
+        actCountry = findViewById(R.id.actCountry);
+        stateSpinner = findViewById(R.id.stateSpinner);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        cbCoding = findViewById(R.id.cbCoding);
+        cbSinging = findViewById(R.id.cbSinging);
+        cbGaming = findViewById(R.id.cbGaming);
+        rgGender = findViewById(R.id.rgGender);
+        married = findViewById(R.id.married);
+
         ArrayAdapter<String> countriesArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries);
         final ArrayAdapter<String> IndiaStatesArrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, indiaStates);
         final ArrayAdapter<String> usaStatesArrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, usaStates);
         final ArrayAdapter<String> canadaStatesArrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, canadaStates);
-        actCountry = findViewById(R.id.actCountry);
-        stateSpinner = findViewById(R.id.stateSpinner);
+
         actCountry.setAdapter(countriesArrayAdapter);
-        actCountry.setThreshold(0);
         actCountry.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast.makeText(getApplicationContext(), adapterView.getItemAtPosition(i) + " Selected ", Toast.LENGTH_SHORT).show();
-                String selectedCountry = adapterView.getItemAtPosition(i).toString();
+                selectedCountry = adapterView.getItemAtPosition(i).toString();
                 switch (selectedCountry) {
                     case "India":
                         stateSpinner.setAdapter(IndiaStatesArrayAdapter);
@@ -48,6 +76,47 @@ public class Practical4 extends AppCompatActivity {
                     default:
                         break;
                 }
+            }
+        });
+
+        stateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedState = adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                selectedState = "Undefined";
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Practical4.this, Practical4Display.class);
+                intent.putExtra("Name", etName.getText().toString());
+                intent.putExtra("DOB", etDOB.getText().toString());
+                intent.putExtra("Age", etAge.getText().toString());
+                intent.putExtra("Phone", etPhone.getText().toString());
+                intent.putExtra("Email", etEmail.getText().toString());
+                intent.putExtra("State", selectedState);
+                intent.putExtra("Country", selectedCountry);
+                selectedGender = findViewById(rgGender.getCheckedRadioButtonId());
+                intent.putExtra("Gender", selectedGender.getText().toString());
+                if (cbCoding.isChecked())
+                    selectedHobbies += cbCoding.getText().toString() + " ";
+                if (cbGaming.isChecked())
+                    selectedHobbies += cbGaming.getText().toString() + " ";
+                if (cbSinging.isChecked())
+                    selectedHobbies += cbSinging.getText().toString();
+                intent.putExtra("Hobbies", selectedHobbies);
+                if (married.isChecked())
+                    intent.putExtra("Married", "Yes");
+                else
+                    intent.putExtra("Married", "No");
+                if (intent != null)
+                    startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/arafat_213/mcwclabpracticals/Practical4Display.java
@@ -0,0 +1,39 @@
+package com.example.arafat_213.mcwclabpracticals;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Practical4Display extends AppCompatActivity {
+
+    private TextView tvName, tvAge, tvDOB, tvEmail, tvPhone, tvHobbies, tvGender, tvCountry, tvState, tvMarried;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical4_display);
+        tvName = findViewById(R.id.tvName);
+        tvAge = findViewById(R.id.tvAge);
+        tvDOB = findViewById(R.id.tvDOB);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvPhone = findViewById(R.id.tvPhone);
+        tvHobbies = findViewById(R.id.tvHobbies);
+        tvGender = findViewById(R.id.tvGender);
+        tvCountry = findViewById(R.id.tvCountry);
+        tvState = findViewById(R.id.tvState);
+        tvMarried = findViewById(R.id.tvMarried);
+
+        Intent intent = getIntent();
+        tvName.setText("Name : " + intent.getStringExtra("Name"));
+        tvAge.setText("Age : " + intent.getStringExtra("Age"));
+        tvDOB.setText("Date of birth : " + intent.getStringExtra("DOB"));
+        tvEmail.setText("Email : " + intent.getStringExtra("Email"));
+        tvPhone.setText("Phone : " + intent.getStringExtra("Phone"));
+        tvGender.setText("Gender : " + intent.getStringExtra("Gender"));
+        tvHobbies.setText("Hobbies : " + intent.getStringExtra("Hobbies"));
+        tvCountry.setText("Cuntry : " + intent.getStringExtra("Country"));
+        tvState.setText("State : " + intent.getStringExtra("State"));
+        tvMarried.setText("Married ?  : " + intent.getStringExtra("Married"));
+    }
+}
