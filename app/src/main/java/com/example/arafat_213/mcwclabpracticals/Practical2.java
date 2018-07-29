8cc3e2e81b70d2c122c4248331b808b261d351f5
arafat-213 <tai.arafat.at@gmail.com>
2018-07-29 17:29:03 +0530

    HAD TO change Toast from "Hello world" to "Hello, welcome to android lab" in Practical 2 (-_-)

--- app/src/main/java/com/example/arafat_213/mcwclabpracticals/Practical2.java
+++ app/src/main/java/com/example/arafat_213/mcwclabpracticals/Practical2.java
@@ -14,6 +14,6 @@ public class Practical2 extends AppCompatActivity {
     }
 
     public void helloWorld(View view) {
-        Toast.makeText(Practical2.this, "Helllo world", Toast.LENGTH_LONG).show();
+        Toast.makeText(Practical2.this, "Helllo, Welcome to android lab", Toast.LENGTH_LONG).show();
     }
 }
